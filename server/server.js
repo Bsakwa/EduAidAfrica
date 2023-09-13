e9e44b0da7c794913cb71cc81e43fde276291dc3
Bsakwa <sakwabrian23@gmail.com>
2023-09-13 11:28:03 +0300

    Changed the server port to 5000

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const connectToDatabase = require('./db'); // Import the database configuration
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Connect to the database
 connectToDatabase();
