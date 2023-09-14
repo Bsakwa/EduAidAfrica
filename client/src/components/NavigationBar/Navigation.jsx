c22d42d798886bd5e1b3f9547e7f7cb0c4a28e73
Eric-Obeng <obengeric.3958@gmail.com>
2023-09-14 09:32:34 +0000

    App.jsx updated

---
+++ client/src/components/NavigationBar/Navigation.jsx
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+const NavigationBar = () => {
+  return (
+    <div className="navigation">
+      <div className="navigation__logo">
+        <span>EduAidAfrica</span>
+      </div>
+    </div>
+  );
+};
+
+export default NavigationBar;
+=======
+const Navigation = () => {
+
+}
+
+export default Navigation
+>>>>>>> 0233089aed877d3d7ff5c1593fabd07831d49c1b
