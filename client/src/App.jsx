8a31f8e8daf2cd0c05fe8f82a34bf3e3650cd563
Eric-Obeng <obengeric.3958@gmail.com>
2023-09-14 06:51:24 +0000

    App.jsx updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,15 @@
-merging
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import NavigationBar from "./components/NavigationBar/Navigation";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <NavigationBar />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
