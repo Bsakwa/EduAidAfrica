206fe70d933121e06e9489ba6ff6e44af14774f5
Bsakwa <sakwabrian23@gmail.com>
2023-09-15 17:06:04 +0300

    Added 0.0.0.0 as default address for the server

--- server/server.js
+++ server/server.js
@@ -18,6 +18,6 @@ app.use(express.json()); // Parse JSON bodies
 routes(app);
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
